9c783c5250805be7f8c5566a347dd33369cf03ed
Niklas K <drdaxxy@googlemail.com>
2018-04-08 02:24:12 +0200

    rich text support in disassembly, bold addresses

--- src/gui/disassemblyitemdelegate.cpp
+++ src/gui/disassemblyitemdelegate.cpp
@@ -1,15 +1,76 @@
 #include "disassemblyitemdelegate.h"
 #include "disassemblymodel.h"
 #include <QPainter>
+#include <QTextDocument>
+#include <QApplication>
+#include <QAbstractTextDocumentLayout>
 
 void DisassemblyItemDelegate::paint(QPainter* painter,
                                     const QStyleOptionViewItem& option,
                                     const QModelIndex& index) const {
-  if (index.column() == (int)DisassemblyModel::ColumnType::Breakpoint) {
-    // TODO parameterize
-    painter->fillRect(option.rect, QColor("#d3b892"));
-  } else {
-    QStyledItemDelegate::paint(painter, option, index);
-    return;
+  QString richText = richTextFor(index);
+  if (!richText.isEmpty()) {
+    return paintRichText(painter, option, index, richText);
   }
+
+  switch ((DisassemblyModel::ColumnType)index.column()) {
+    case DisassemblyModel::ColumnType::Breakpoint: {
+      // TODO parameterize
+      painter->fillRect(option.rect, QColor("#d3b892"));
+      break;
+    }
+    default: {
+      QStyledItemDelegate::paint(painter, option, index);
+      break;
+    }
+  }
+}
+
+QString DisassemblyItemDelegate::richTextFor(const QModelIndex& index) const {
+  switch ((DisassemblyModel::ColumnType)index.column()) {
+    case DisassemblyModel::ColumnType::Address: {
+      return "<b>" + index.data().toString() + "</b>  ";
+    }
+    default: { return QString(); }
+  }
+}
+
+// https://stackoverflow.com/a/1956781
+void DisassemblyItemDelegate::paintRichText(QPainter* painter,
+                                            const QStyleOptionViewItem& option,
+                                            const QModelIndex& index,
+                                            const QString& richText) const {
+  QStyleOptionViewItemV4 options = option;
+  initStyleOption(&options, index);
+
+  painter->save();
+
+  QTextDocument doc;
+  // TODO global unified styling
+  doc.setDefaultStyleSheet(
+      "body { font-family: 'Lucida Console'; font-size: 9pt; }");
+  doc.setHtml("<body>" + richText + "</body>");
+
+  const QWidget* widget = options.widget;
+  QStyle* style = widget ? widget->style() : QApplication::style();
+  options.text = "";
+  style->drawControl(QStyle::CE_ItemViewItem, &options, painter, widget);
+
+  painter->translate(options.rect.left(), options.rect.top());
+  QRect clip(0, 0, options.rect.width(), options.rect.height());
+  doc.drawContents(painter, clip);
+
+  painter->restore();
+}
+
+QSize DisassemblyItemDelegate::sizeHint(const QStyleOptionViewItem& option,
+                                        const QModelIndex& index) const {
+  QString richText = richTextFor(index);
+  if (richText.isEmpty()) return QStyledItemDelegate::sizeHint(option, index);
+
+  QTextDocument doc;
+  doc.setDefaultStyleSheet(
+      "body { font-family: 'Lucida Console'; font-size: 9pt; }");
+  doc.setHtml("<body>" + richText + "</body>");
+  return QSize(doc.idealWidth(), doc.size().height());
 }
--- src/gui/disassemblyitemdelegate.h
+++ src/gui/disassemblyitemdelegate.h
@@ -10,4 +10,12 @@ class DisassemblyItemDelegate : public QStyledItemDelegate {
 
   void paint(QPainter* painter, const QStyleOptionViewItem& option,
              const QModelIndex& index) const override;
+  QSize sizeHint(const QStyleOptionViewItem& option,
+                 const QModelIndex& index) const override;
+
+ private:
+  void paintRichText(QPainter* painter, const QStyleOptionViewItem& option,
+                     const QModelIndex& index, const QString& richText) const;
+
+  QString richTextFor(const QModelIndex& index) const;
 };
