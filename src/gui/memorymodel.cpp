e5ec9638f0c0e5445c3507c33780d8d2e1fc9f9b
Niklas K <drdaxxy@googlemail.com>
2018-04-11 13:47:55 +0200

    make var names editable

--- src/gui/memorymodel.cpp
+++ src/gui/memorymodel.cpp
@@ -31,18 +31,29 @@ QVariant MemoryModel::headerData(int section, Qt::Orientation orientation,
 }
 
 Qt::ItemFlags MemoryModel::flags(const QModelIndex &index) const {
-  // TODO: editable
-  return QAbstractTableModel::flags(index);
+  auto origFlags = QAbstractTableModel::flags(index);
+  if (!index.isValid()) return origFlags;
+  if ((ColumnType)index.column() == ColumnType::Name)
+    return origFlags | Qt::ItemIsEditable;
+  return origFlags;
 }
 
 bool MemoryModel::setData(const QModelIndex &index, const QVariant &value,
                           int role) {
-  // TODO: editable
+  if (index.isValid() && role == Qt::EditRole &&
+      (ColumnType)index.column() == ColumnType::Name) {
+    VariableRefType type;
+    int var;
+    std::tie(type, var) = varForIndex(index);
+    dApp->project()->setVarName(value.toString(), type, var);
+    return true;
+  }
   return false;
 }
 
 QVariant MemoryModel::data(const QModelIndex &index, int role) const {
-  if (role != Qt::DisplayRole) return QVariant();
+  if (!index.isValid() || (role != Qt::DisplayRole && role != Qt::EditRole))
+    return QVariant();
   VariableRefType varType;
   int var;
   std::tie(varType, var) = varForIndex(index);
@@ -58,7 +69,11 @@ QVariant MemoryModel::data(const QModelIndex &index, int role) const {
       break;
     }
     case ColumnType::Name: {
-      return QVariant(dApp->project()->getVarName(varType, var));
+      QString name = dApp->project()->getVarName(varType, var);
+      if (role == Qt::DisplayRole && name != QString("%1").arg(var)) {
+        return QVariant(QString("%1 (%2)").arg(name).arg(var));
+      }
+      return QVariant(name);
       break;
     }
     default:
